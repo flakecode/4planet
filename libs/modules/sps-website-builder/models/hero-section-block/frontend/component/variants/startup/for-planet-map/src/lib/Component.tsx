a4888b53bff3110f14a9f1a66ac15001484f9705
Llitvinchuk <62562116+Llitvinchuk@users.noreply.github.com>
2024-04-13 16:30:30 +0300

    add map

--- libs/modules/sps-website-builder/models/hero-section-block/frontend/component/variants/startup/for-planet-map/src/lib/Component.tsx
+++ libs/modules/sps-website-builder/models/hero-section-block/frontend/component/variants/startup/for-planet-map/src/lib/Component.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { IComponentPropsExtended } from "./interface";
 import ReactMarkdown from "react-markdown";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+
+const error = console.error;
+console.error = (...args: any) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
 
 export function Component(props: IComponentPropsExtended) {
-  const apiKey = process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY;
-  console.log(`🚀 ~ Component ~ apiKey:`, apiKey);
   return (
     <div
       data-module="sps-website-builder"
@@ -22,10 +27,35 @@ export function Component(props: IComponentPropsExtended) {
             </h1>
           ) : null}
           {props.data?.description ? (
-            <ReactMarkdown className="mx-auto text-base md:text-lg max-w-4xl">
+            <ReactMarkdown className="mx-auto text-base md:text-lg max-w-4xl flex flex-col gap-7">
               {props.data?.description}
             </ReactMarkdown>
           ) : null}
+        </div>
+        <div className="w-full mt-10 lg:mt-[66px]">
+          <YMaps>
+            <div className="flex justify-center">
+              <Map
+                defaultState={{
+                  center: [52.696873, 88.824296],
+                  zoom: 4,
+                  controls: ["zoomControl", "fullscreenControl"],
+                }}
+                modules={[
+                  "control.ZoomControl",
+                  "control.FullscreenControl",
+                  "control.TrafficControl",
+                ]}
+                width={1200}
+                height={709}
+              >
+                <Placemark defaultGeometry={[51.204022, 51.370033]} />
+                <Placemark defaultGeometry={[56.009849, 92.850352]} />
+                <Placemark defaultGeometry={[51.834604, 107.584326]} />
+                <Placemark defaultGeometry={[47.918191, 106.917745]} />
+              </Map>
+            </div>
+          </YMaps>
         </div>
       </div>
     </div>
--- libs/modules/sps-website-builder/models/navbar-block/frontend/component/variants/startup/for-planet-default/src/lib/Component.tsx
+++ libs/modules/sps-website-builder/models/navbar-block/frontend/component/variants/startup/for-planet-default/src/lib/Component.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Component as Button } from "@sps/sps-website-builder-models-button-frontend-component";
 import { Disclosure } from "@headlessui/react";
 import { Bars2Icon, Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
-import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { Component as Logotype } from "@sps/sps-website-builder-models-logotype-frontend-component";
 import { IComponentPropsExtended } from "./interface";
